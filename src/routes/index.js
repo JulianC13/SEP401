368b20c642cc4bdd5b5a0ac4b7f7f09cf70d1ba4
Julian Camilo Gomez <jc.gomez18@uniandes.edu.co>
2022-08-10 22:16:44 +1000

    bug

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,9 @@ var router = expess.Router();
 const {loginView } = require('../controllers/loginController');
 const {menuView } = require('../controllers/menuController');
 const {appointmentView } = require('../controllers/appointmentController');
-const {infoView } = require('../controllers/userInfo');
 
 router.get('/menu', menuView);
 router.get('/', loginView);
 router.get('/appointment/:appointmentId', appointmentView);
-router.get('/userInfo', infoView);
 
 module.exports = router;
